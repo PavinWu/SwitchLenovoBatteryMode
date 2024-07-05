8c7ea561aed5a3bd964949fe8d9f0fc0c74aa910
Pavin Wu <pavin.wu@gmail.com>
2024-07-06 09:29:14 +1000

    Use /etc/crontab again, Move script to /usr/local/bin (reddit post says systemd doesn't like running system script from home: https://www.reddit.com/r/Fedora/comments/1aybp5x/cant_run_script_on_boot/

--- setupCronjob.sh
+++ setupCronjob.sh
@@ -13,4 +13,8 @@ escapedScriptPath="${scriptPath//\//\\/}"
 sed "s/PATH_TO_REPLACE/$escapedScriptPath/" $fullscriptPath > $fullNewScriptPath
 chmod +x $fullNewScriptPath
 
-mv $fullNewScriptPath /etc/cron.daily/
+# TODO not run??
+# mv $fullNewScriptPath /etc/cron.daily/
+
+mv $fullNewScriptPath /usr/local/bin
+echo "@reboot root sleep 60 && /usr/local/bin/$newScriptName" >> /etc/crontab
